fc117d378ee78714492a26040e5b29d454447134
remiconnesson <remiconnesson2@gmail.com>
2026-05-21 13:12:01 +0100

    Update description in metadata for layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import './globals.css';
 export const metadata = {
 	title: 'Storyblok + Vercel Demo',
 	description:
-		'Visual Storyblok authoring with a Next.js frontend delivered on Vercel.',
+		'Visual Storyblok authoring with a Next.js frontend delivered on Vercel!',
 };
 
 export default function RootLayout({ children }) {
